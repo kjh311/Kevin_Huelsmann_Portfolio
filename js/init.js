3e398f2b85baef515835fdc7b4d49d3d5e9c0fef
Kevin Huelsmann <kjh311@gmail.com>
2016-09-30 09:35:09 -0700

    animated pic

--- js/init.js
+++ js/init.js
@@ -1,9 +1,17 @@
 (function($){
   $(function(){
 
+
+
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     // $('.collapsible').collapsible();
 
+
+
+
+
   }); // end of document ready
+
+
 })(jQuery); // end of jQuery name space
