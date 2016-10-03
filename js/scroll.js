7c76d34a7ff83c3a4d07712e1d76f510f291d5b5
Kevin Huelsmann <kjh311@gmail.com>
2016-10-03 14:28:04 -0700

    cards

--- js/scroll.js
+++ js/scroll.js
@@ -1,41 +1,39 @@
 $( document ).ready(function() {
-var options = [
-    {selector: '.education-container', offset: 200, callback: '$(this).addClass("animated jello")' },
-];
-Materialize.scrollFire(options);
+// var options = [
+//     {selector: '.education-container', offset: 200, callback: '$('.education-container').addClass("animated jello")' },
+// ];
+// Materialize.scrollFire(options);
 
 
   // SCROLLING / ANIMATION:
-    $(function(){
-            // Ends Animation:
-      var vendor          = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    // $(function(){
+    //         // Ends Animation:
+    //   var vendor          = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
-      var bounce          = 'animated bounce';
-      var rollIn          = "animated rollIn";
-      var bounceInDown    = "animated bounceInDown";
-      var flip            = 'animated flip';
-      var flipInX         = 'animated flipInX';
-      var lightSpeedIn    = "animated lightSpeedIn";
-      var zoomIn          = "animated zoomIn";
-      var bounceOutLeft   = "animated bounceOutLeft";
-      var bounceOutDown   = "animated bounceOutDown";
-      var shake           = "animated shake";
-      var bounceInRight   = "animated bounceInRight";
-      var zoomOutDown     = "animated zoomOutDown";
-      var hinge           = "animated hinge";
-      var rubberBand      = "animated rubberBand";
-      var rotateOut       = "animated rotateOut";
-      var jello           = "animated jello";
-      var fadeOut         = "animated fadeOut";
+    //   var bounce          = 'animated bounce';
+    //   var rollIn          = "animated rollIn";
+    //   var bounceInDown    = "animated bounceInDown";
+    //   var flip            = 'animated flip';
+    //   var flipInX         = 'animated flipInX';
+    //   var lightSpeedIn    = "animated lightSpeedIn";
+    //   var zoomIn          = "animated zoomIn";
+    //   var bounceOutLeft   = "animated bounceOutLeft";
+    //   var bounceOutDown   = "animated bounceOutDown";
+    //   var shake           = "animated shake";
+    //   var bounceInRight   = "animated bounceInRight";
+    //   var zoomOutDown     = "animated zoomOutDown";
+    //   var hinge           = "animated hinge";
+    //   var rubberBand      = "animated rubberBand";
+    //   var rotateOut       = "animated rotateOut";
+    //   var jello           = "animated jello";
+    //   var fadeOut         = "animated fadeOut";
 
  // $('.education-container').addClass(jello);
 
 
          $('.education-container').waypoint(function(){
-            $('.education-container').addClass(bounceOutDown).one(vendor, function(){
-              // $(this).removeClass(flip);
-            });
-        },{offset:'10%'})
+            $('.education-container').addClass("animated jello");
+           },{offset:'10%'})
 
 
 //         $('.education-container').waypoint(function(direction) {
@@ -45,5 +43,5 @@ Materialize.scrollFire(options);
 
 
     // });
-});
+// });
 });
