a82e19ae9b8e27a7726841497d7396c3edb58dc7
Kevin Huelsmann <kjh311@gmail.com>
2016-11-01 22:11:53 -0700

    sad

--- js/init.js
+++ js/init.js
@@ -11,20 +11,11 @@
 
 
 
-    $("#nav-mobile").click(function(){
-      $("#nav-mobile").addClass("animated slideOutLeft");
-      $('#sidenav-overlay').fadeTo( "slow" , 0, function() {
-    // Animation complete.
-    });
-      $('#sidenav-overlay').addClass('z');
-      function explode(){
-        $("#nav-mobile").removeClass("animated slideOutLeft");
-        $("#nav-mobile").css({ left: -250});
-      }
-      setTimeout(explode, 1000);
+    $("#nav-mobile").on("click", function() {
+    $("#sidenav-overlay").trigger("click");
+    return false;
+});
 
-
-    });
 
 
 
