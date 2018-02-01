69d2869cf8521e58c54eb7303dbcbcf124e9030a
Kevin <kjh311@gmail.com>
2018-02-01 10:44:53 -0800

    asdf

--- js/init.js
+++ js/init.js
@@ -11,33 +11,114 @@
 // }, 1050);
 
 
-var kevPortfolioText = document.getElementById("kevPortfolioText");
-TweenLite.to(kevPortfolioText, 1, {opacity:1, left:20, ease:Power2.easeOut, delay:0.8});
+var kevPortfolioText2 = document.getElementById("kevPortfolioText2");
+TweenLite.to(kevPortfolioText2, 1, {opacity:1, left:0, ease:Power2.easeOut, delay:0.8});
+
+var portText1 = document.getElementById("portText1");
+TweenLite.to(portText1, 0.7, {opacity:1, left:80, ease:Power3.easeOut, delay:0.5});
+
+var portText2 = document.getElementById("portText2");
+TweenLite.to(portText2, 0.7, {opacity:1, left:115, ease:Power3.easeOut, delay:0.55});
+
+var portText3 = document.getElementById("portText3");
+TweenLite.to(portText3, 0.7, {opacity:1, left:145, ease:Power3.easeOut, delay:0.6});
+
+var portText4 = document.getElementById("portText4");
+TweenLite.to(portText4, 0.7, {opacity:1, left:175, ease:Power3.easeOut, delay:0.65});
+
+var portText5 = document.getElementById("portText5");
+TweenLite.to(portText5, 0.7, {opacity:1, left:190, ease:Power3.easeOut, delay:0.7});
+
+var portText6 = document.getElementById("portText6");
+TweenLite.to(portText6, 0.7, {opacity:1, left:240, ease:Power3.easeOut, delay:0.75});
+
+var portText7 = document.getElementById("portText7");
+TweenLite.to(portText7, 0.7, {opacity:1, left:280, ease:Power3.easeOut, delay:0.8});
+
+var portText8 = document.getElementById("portText8");
+TweenLite.to(portText8, 0.7, {opacity:1, left:315, ease:Power3.easeOut, delay:0.85});
+
+var portText9 = document.getElementById("portText9");
+TweenLite.to(portText9, 0.7, {opacity:1, left:345, ease:Power3.easeOut, delay:0.9});
+
+var portText10 = document.getElementById("portText10");
+TweenLite.to(portText10, 0.7, {opacity:1, left:360, ease:Power3.easeOut, delay:0.95});
+
+var portText11 = document.getElementById("portText11");
+TweenLite.to(portText11, 0.7, {opacity:1, left:390, ease:Power3.easeOut, delay:1});
+
+var portText12 = document.getElementById("portText12");
+TweenLite.to(portText12, 0.7, {opacity:1, left:440, ease:Power3.easeOut, delay:1.05});
+
+var portText13 = document.getElementById("portText13");
+TweenLite.to(portText13, 0.7, {opacity:1, left:470, ease:Power3.easeOut, delay:1.1});
+
+var portText14 = document.getElementById("portText14");
+TweenLite.to(portText14, 0.7, {opacity:1, left:500, ease:Power3.easeOut, delay:1.15});
+
+var portText15 = document.getElementById("portText15");
+TweenLite.to(portText15, 0.7, {opacity:1, left:550, ease:Power3.easeOut, delay:1.2});
+
+var portText16 = document.getElementById("portText16");
+TweenLite.to(portText16, 0.7, {opacity:1, left:585, ease:Power3.easeOut, delay:1.25});
+
+var portText17 = document.getElementById("portText17");
+TweenLite.to(portText17, 0.7, {opacity:1, left:620, ease:Power3.easeOut, delay:1.3});
+
+var portText18 = document.getElementById("portText18");
+TweenLite.to(portText18, 0.7, {opacity:1, left:645, ease:Power3.easeOut, delay:1.35});
+
+var portText19 = document.getElementById("portText19");
+TweenLite.to(portText19, 0.7, {opacity:1, left:670, ease:Power3.easeOut, delay:1.4});
+
+var portText20 = document.getElementById("portText20");
+TweenLite.to(portText20, 0.7, {opacity:1, left:700, ease:Power3.easeOut, delay:1.45});
+
+var portText21 = document.getElementById("portText21");
+TweenLite.to(portText21, 0.7, {opacity:1, left:735, ease:Power3.easeOut, delay:1.5});
+
+var portText22 = document.getElementById("portText22");
+TweenLite.to(portText22, 0.7, {opacity:1, left:755, ease:Power3.easeOut, delay:1.55});
+
+var portText23 = document.getElementById("portText23");
+TweenLite.to(portText23, 0.7, {opacity:1, left:775,  ease:Power3.easeOut, delay:1.6});
+
+
+
+
+
+
+
 
 var aboutMeText = document.getElementById("aboutMeText");
-TweenLite.to(aboutMeText, 1, {opacity:1, ease:Power2.easeOut, delay:2});
+TweenLite.to(aboutMeText, 1, {opacity:1, top: -10, ease:Power2.easeOut, delay:2.3});
 
 
-var link1 = document.getElementById("link1");
-TweenLite.to(link1, 1, {opacity:1, ease:Power2.easeOut, delay:0.4});
 
-var link2 = document.getElementById("link2");
-TweenLite.to(link2, 1, {opacity:1, ease:Power2.easeOut, delay:0.5});
 
-var link3 = document.getElementById("link3");
-TweenLite.to(link3, 1, {opacity:1, ease:Power2.easeOut, delay:0.6});
+// var link1 = document.getElementById("link1");
+// TweenLite.to(link1, 1, {opacity:1, ease:Power2.easeOut, delay:0.5});
 
-var link4 = document.getElementById("link4");
-TweenLite.to(link4, 1, {opacity:1, ease:Power2.easeOut, delay:0.7});
+// var link2 = document.getElementById("link2");
+// TweenLite.to(link2, 1, {opacity:1, ease:Power2.easeOut, delay:0.5});
+
+// var link3 = document.getElementById("link3");
+// TweenLite.to(link3, 1, {opacity:1, ease:Power2.easeOut, delay:0.5});
+
+// var link4 = document.getElementById("link4");
+// TweenLite.to(link4, 1, {opacity:1, ease:Power2.easeOut, delay:0.5});
+
+// var link4 = document.getElementById("link4");
+// TweenLite.to(link4, 1, {opacity:1, ease:Power2.easeOut, delay:0.5});
 
 var kevPic = document.getElementById("kevPic");
-TweenLite.to(kevPic, 1, {opacity:1, ease:Power2.easeOut, delay:1.5});
+TweenLite.to(kevPic, 1.5, {opacity:1, ease:Power2.easeOut, delay:2});
 
 var background1 = document.getElementById("background1");
-TweenLite.to(background1, 1, {opacity:1, ease:Power2.easeOut});
+TweenLite.to(background1, 1, {opacity:1, ease:Power1.easeOut});
 
-var section1 = document.getElementById("section1");
-TweenLite.to(section1, 1, {opacity:1, ease:Power2.easeOut, delay:2.3});
+// var section1 = document.getElementById("section1");
+// TweenLite.to(section1, 1, {opacity:1, ease:Power2.easeOut, delay:2.3});
 
 
 
